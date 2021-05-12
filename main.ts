07fa0554993269bfa5c051245c1b84bdf5447a54
guada-123 <83685388+guada-123@users.noreply.github.com>
2021-05-12 11:54:47 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,39 @@
+basic.showLeds(`
+    # # # # .
+    # . . . .
+    # . # # #
+    # . . . #
+    # # # # #
+    `)
+basic.showNumber(500)
+basic.showLeds(`
+    # . . . #
+    # . . . #
+    # . . . #
+    # . . . #
+    . # # # .
+    `)
+basic.showLeds(`
+    . . # . .
+    . # . # .
+    # . . . #
+    # # # # #
+    # . . . #
+    `)
+basic.showNumber(500)
+basic.showLeds(`
+    # # # # .
+    # . . . #
+    # . . . #
+    # . . . #
+    # # # # .
+    `)
+basic.showNumber(500)
+basic.showLeds(`
+    . . # . .
+    . # . # .
+    # . . . #
+    # # # # #
+    # . . . #
+    `)
+basic.showNumber(500)
